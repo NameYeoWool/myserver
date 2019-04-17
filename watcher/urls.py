86e7b38a639955262903f7b09e0642bf6f3cecd2
YeoWool <gkaudrl542@daum.net>
2019-04-17 22:11:17 +0900

    added views to creat/edit watcherapp inside.

--- watcher/urls.py
+++ watcher/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.room_list, name='room_list'),
-    path('room/<str:address>/', views.room_detail, name='room_detail')
+    path('room/new/', views.room_new, name='room_new'),
+    path('room/detail/<str:address>/', views.room_detail, name='room_detail'),
+    path('room/edit/<str:address>/', views.room_edit, name='room_edit'),
 ]
--- watcher/views.py
+++ watcher/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Room
+from .forms import RoomForm
+from django.shortcuts import redirect
 
 def room_list(request):
     rooms = Room.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
@@ -10,3 +12,31 @@ def room_list(request):
 def room_detail(request, address):
     room = get_object_or_404(Room, address=address)
     return render(request, 'watcher/room_detail.html', {'room':room})
+
+
+def room_new(request):
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            room = form.save(commit=False)
+            room.created_date = timezone.now()
+            room.save()
+            return redirect('room_detail', address=room.address)
+    else:
+        form = RoomForm()
+
+    return render(request, 'watcher/room_edit.html',{'form':form})
+
+
+def room_edit(request, address):
+    room = get_object_or_404(Room, address=address)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            room = form.save(commit=False)
+            room.created_date = timezone.now()
+            room.save()
+            return redirect('room_detail', address=room.address)
+    else:
+        form = RoomForm(instance=room)
+    return render(request, 'watcher/room_edit.html', {'form': form})
